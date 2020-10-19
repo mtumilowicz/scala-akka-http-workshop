739c5cb7fce08e0b26fb5603dec98bb2a6828487
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-10-19 21:57:12 +0200

    routes - handling http statuses

--- src/main/scala/app/gateway/UserRoutes.scala
+++ src/main/scala/app/gateway/UserRoutes.scala
@@ -3,25 +3,16 @@ package app.gateway
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import app.domain.User
 import app.gateway.in.NewUserApiInput
 
-//#import-json-formats
-//#user-routes-class
 class UserRoutes(userHandler: UserHandler) {
 
-  //#user-routes-class
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import app.infrastructure.JsonFormats._
-  //#import-json-formats
 
-  //#all-routes
-  //#users-get-post
-  //#users-get-delete
   val userRoutes: Route =
     pathPrefix("users") {
       concat(
-        //#users-get-delete
         pathEnd {
           concat(
             get {
@@ -35,28 +26,19 @@ class UserRoutes(userHandler: UserHandler) {
               }
             })
         },
-        //#users-get-delete
-        //#users-get-post
         path(Segment) { name =>
           concat(
             get {
-              //#retrieve-user-info
-              rejectEmptyResponse {
-                onSuccess(userHandler.getUser(name)) { response =>
-                  complete(response)
-                }
+              onSuccess(userHandler.getUser(name)) {
+                case Some(value) => complete(value)
+                case None => complete(StatusCodes.NotFound, s"user with given name: $name not found")
               }
-              //#retrieve-user-info
             },
             delete {
-              //#users-delete-logic
               onSuccess(userHandler.deleteUser(name)) { performed =>
                 complete((StatusCodes.OK, performed))
               }
-              //#users-delete-logic
             })
         })
-      //#users-get-delete
     }
-  //#all-routes
 }
