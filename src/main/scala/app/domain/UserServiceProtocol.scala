ca71181fa8984cdf1b3bff24b56a6e8cb2c73993
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-10-20 09:22:36 +0200

    UserService and UserServiceProtocol separation

--- src/main/scala/app/domain/UserServiceProtocol.scala
+++ src/main/scala/app/domain/UserServiceProtocol.scala
@@ -1,29 +1,6 @@
 package app.domain
 
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior}
-import app.domain.UserServiceProtocol._
-
-class UserService(repository: UserRepository) {
-
-  def behaviour(): Behavior[Command] =
-    Behaviors.receiveMessage {
-      case GetUsers(replyTo) =>
-        replyTo ! repository.findAll
-        Behaviors.same
-      case CreateUser(input, replyTo) =>
-        replyTo ! ActionPerformed(s"User ${input.name} created.")
-        repository.save(User.createFrom(input))
-        Behaviors.same
-      case GetUser(name, replyTo) =>
-        replyTo ! repository.findById(name)
-        Behaviors.same
-      case DeleteUser(name, replyTo) =>
-        replyTo ! ActionPerformed(s"User $name deleted.")
-        repository.delete(name)
-        Behaviors.same
-    }
-}
+import akka.actor.typed.ActorRef
 
 object UserServiceProtocol {
 
