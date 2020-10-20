3c0497dfb136d70b62580bfd3245d443fe42be68
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-10-20 09:37:20 +0200

    UserServiceConfiguration

--- src/main/scala/app/domain/UserServiceProtocol.scala
+++ src/main/scala/app/domain/UserServiceProtocol.scala
@@ -1,6 +1,7 @@
 package app.domain
 
-import akka.actor.typed.ActorRef
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
 
 object UserServiceProtocol {
 
@@ -15,5 +16,21 @@ object UserServiceProtocol {
   final case class DeleteUser(name: String, replyTo: ActorRef[ActionPerformed]) extends Command
 
   final case class ActionPerformed(description: String)
+
+  def behaviour(userService: UserService): Behavior[Command] =
+    Behaviors.receiveMessage {
+      case GetUsers(replyTo) =>
+        replyTo ! userService.findAll()
+        Behaviors.same
+      case CreateUser(input, replyTo) =>
+        replyTo ! userService.save(input)
+        Behaviors.same
+      case GetUser(name, replyTo) =>
+        replyTo ! userService.findById(name)
+        Behaviors.same
+      case DeleteUser(name, replyTo) =>
+        replyTo ! userService.delete(name)
+        Behaviors.same
+    }
 
 }
