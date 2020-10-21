cdf4e5feb75907daf25816959dc9d245d4f58e8d
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-10-21 11:26:21 +0200

    bbehaviour of UserService

--- src/main/scala/app/domain/UserService.scala
+++ src/main/scala/app/domain/UserService.scala
@@ -1,5 +1,7 @@
 package app.domain
 
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
 import app.domain.UserServiceProtocol._
 
 class UserService(repository: UserRepository) {
@@ -18,4 +20,19 @@ class UserService(repository: UserRepository) {
     ActionPerformed(s"User $name deleted.")
   }
 
+  def behaviour: Behavior[Command] =
+    Behaviors.receiveMessage {
+      case GetUsers(replyTo) =>
+        replyTo ! findAll()
+        Behaviors.same
+      case CreateUser(input, replyTo) =>
+        replyTo ! save(input)
+        Behaviors.same
+      case GetUser(name, replyTo) =>
+        replyTo ! findById(name)
+        Behaviors.same
+      case DeleteUser(name, replyTo) =>
+        replyTo ! delete(name)
+        Behaviors.same
+    }
 }
--- src/main/scala/app/domain/UserServiceProtocol.scala
+++ src/main/scala/app/domain/UserServiceProtocol.scala
@@ -1,7 +1,6 @@
 package app.domain
 
-import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorRef
 
 object UserServiceProtocol {
 
@@ -16,21 +15,5 @@ object UserServiceProtocol {
   final case class DeleteUser(name: String, replyTo: ActorRef[ActionPerformed]) extends Command
 
   final case class ActionPerformed(description: String)
-
-  def behaviour(userService: UserService): Behavior[Command] =
-    Behaviors.receiveMessage {
-      case GetUsers(replyTo) =>
-        replyTo ! userService.findAll()
-        Behaviors.same
-      case CreateUser(input, replyTo) =>
-        replyTo ! userService.save(input)
-        Behaviors.same
-      case GetUser(name, replyTo) =>
-        replyTo ! userService.findById(name)
-        Behaviors.same
-      case DeleteUser(name, replyTo) =>
-        replyTo ! userService.delete(name)
-        Behaviors.same
-    }
 
 }
