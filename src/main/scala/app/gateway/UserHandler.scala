7dd72ed1a906b5d2dd673e1c1dd492eb06e9a6ba
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-11-01 21:12:28 +0100

    naming

--- src/main/scala/app/gateway/UserHandler.scala
+++ src/main/scala/app/gateway/UserHandler.scala
@@ -10,30 +10,30 @@ import app.gateway.out.{UserApiOutput, UserApiOutputBuilder, UsersApiOutput, Use
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class UserHandler(userRegistry: ActorRef[UserServiceProtocol.Command])(implicit val system: ActorSystem[_]) {
+class UserHandler(userSevice: ActorRef[UserServiceProtocol.Command])(implicit val system: ActorSystem[_]) {
 
   private implicit val timeout = Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
 
 
   def getUsers(): Future[UsersApiOutput] =
-    userRegistry.ask(GetUsers).map(UsersApiOutputBuilder.fromDomain)
+    userSevice.ask(GetUsers).map(UsersApiOutputBuilder.fromDomain)
 
   def getUserById(id: String): Future[Option[UserApiOutput]] = {
     def toOutput: Option[User] => Option[UserApiOutput] = _.map(UserApiOutputBuilder.fromDomain)
 
-    userRegistry.ask(GetUserById(UserId(id), _)).map(toOutput)
+    userSevice.ask(GetUserById(UserId(id), _)).map(toOutput)
   }
 
   def createUser(input: NewUserInput): Future[UserApiOutput] =
-    userRegistry.ask(CreateUser(input, _)).map(UserApiOutputBuilder.fromDomain)
+    userSevice.ask(CreateUser(input, _)).map(UserApiOutputBuilder.fromDomain)
 
   def replaceUser(input: ReplaceUserInput): Future[Option[UserApiOutput]] = {
     def toOutput: Option[User] => Option[UserApiOutput] = _.map(UserApiOutputBuilder.fromDomain)
 
-    userRegistry.ask(ReplaceUser(input, _)).map(toOutput)
+    userSevice.ask(ReplaceUser(input, _)).map(toOutput)
   }
 
   def deleteUserById(id: String): Future[Option[UserId]] =
-    userRegistry.ask(DeleteUserById(UserId(id), _))
+    userSevice.ask(DeleteUserById(UserId(id), _))
 
 }
