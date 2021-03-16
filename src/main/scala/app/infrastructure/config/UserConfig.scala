13f56e2b4dbf323bed1290d9db0627183cbeeb17
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-03-16 21:14:35 +0100

    refactor

--- src/main/scala/app/infrastructure/config/UserConfig.scala
+++ src/main/scala/app/infrastructure/config/UserConfig.scala
@@ -1,7 +1,7 @@
 package app.infrastructure.config
 
 import app.domain.user.{UserRepository, UserService}
-import app.infrastructure.actor.UserActor
+import app.infrastructure.actor.{UserActor, UserActorWorkshop}
 import app.infrastructure.repository.UserInMemoryRepository
 
 object UserConfig {
@@ -9,8 +9,14 @@ object UserConfig {
   def inMemoryActor(): UserActor =
     actor(inMemoryService())
 
+  def inMemoryActorWorkshop(): UserActor =
+    actorWorkshop(inMemoryService())
+
   def actor(service: UserService): UserActor =
     new UserActor(service)
+
+  def actorWorkshop(service: UserService): UserActor =
+    new UserActorWorkshop(service)
 
   def inMemoryService(): UserService =
     new UserService(inMemoryRepository())
